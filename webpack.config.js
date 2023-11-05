97d94cdcc9023f25fc9da58f33a3f58f00520516
뮤즈티 <dev0019@naver.com>
2023-11-05 17:10:24 +0900

    멀티페이지

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// 개발 모드 감지
+//const isDevMode = process.env.NODE_ENV.includes("dev");
 
 const config = {
-	mode: "none",
+	mode: "development",
+	mode: "development",
+	devServer: {
+		port: 9000,
+		static: {
+			directory: path.resolve(__dirname, "./dist"),
+		},
+		devMiddleware: {
+			index: "index.html",
+			writeToDisk: true,
+		},
+	},
 	entry: "./src/index.js",
 	output: {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "./dist"),
-		publicPath: "dist/",
+		publicPath: "",
+		// clean: {
+		// 	dry: true,
+		// 	keep: /\.css/,
+		// },
 	},
 	module: {
 		rules: [
@@ -30,10 +52,12 @@ const config = {
 			{
 				test: /\.css$/,
 				use: ["style-loader", "css-loader"],
+				//use: [MiniCssExtractPlugin.loader, "css-loader"],
 			},
 			{
 				test: /\.scss$/,
 				use: ["style-loader", "css-loader", "sass-loader"],
+				//use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
 			},
 			{
 				test: /\.js$/,
@@ -48,6 +72,28 @@ const config = {
 			},
 		],
 	},
+	plugins: [
+		new TerserPlugin({}),
+		// new MiniCssExtractPlugin({
+		// 	filename: "style.css",
+		// }),
+		//
+		new CleanWebpackPlugin({
+			cleanOnceBeforeBuildPatterns: [
+				"**/*",
+				path.join(process.cwd(), "build/**/*"),
+			],
+		}),
+
+		//
+		new HtmlWebpackPlugin({
+			title: "Hello World",
+			template: "./src/index.ejs",
+			templateParameters: {
+				title: "EJS Page Check",
+			},
+		}),
+	],
 };
 
 module.exports = config;
